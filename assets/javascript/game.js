baf9307988eb1c2d8d734614c559120fc4464fcd
Hillary Lavin <hillarylavin@Hillarys-MacBook-Pro.local>
2018-03-06 16:10:40 -0500

    Pseudocode in game.js file

---
+++ assets/javascript/game.js
@@ -0,0 +1,20 @@
+// Welcome to the Crystal Kingdom!
+
+// Display 4 crystals on the page (functional as buttons)
+
+// At start of New Game:
+    // Display random number between 19 - 200
+    // Assign random hidden numerical value to each crystal between 1 - 12
+    // Hide player's total score until they click a crystal
+
+// When player clicks a crystal, it will add the assigned numerical value of the crystal to the player's total
+
+// Player wins if they match random number exactly
+
+// Player loses if they go over random number
+
+// Game restarts when player wins or loses and appropriate counter increments
+
+// Wins Counter
+
+// Losses Counter
