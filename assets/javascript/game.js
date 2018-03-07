582020a18cd0ea5952c364bbb5d6275e3c23c5bf
Hillary Lavin <hillarylavin@Hillarys-MacBook-Pro.local>
2018-03-07 14:49:59 -0500

    Start js over again because I can't figure this out

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,26 +12,6 @@ $(document).ready(function() {
     // Display random number between 19 - 200
 
 // Assign random hidden numerical value to each crystal between 1 - 12
-    // Write function that will get a random value from a range of numbers
-    function getRandomValue(min, max) {
-        return Math.random() * (max - min) + min;
-    };
-    // Grab the images and dynamically add data-attribute using result from getRandomValue function
-    // ID selectors
-    var getGem1 = $("#gem1");
-    var getGem2 = $("#gem2");
-    var getGem3 = $("#gem3");
-    var getGem4 = $("#gem4");
-
-
-
-    // I can't figure this one out so far, so I'm going to try a more laborious, but easier to understand method.
-        // for(var i = 0; i < 4; i++) {
-        //     var random = Math.floor((Math.random() * 12) + 1);
-        //     console.log(random);
-
-        //     $(".crystal-gem").attr("data-crystalvalue");
-        // }
 
 // Hide player's total score until they click a crystal
 
